ab7115a2eca43d783b22f121c5273dc7ef228501
Rohan Mani <64933913+rm0052@users.noreply.github.com>
2025-03-12 15:42:27 -0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -7,7 +7,7 @@ import uuid
 import time
 
 # Streamlit App Title
-st.title("Test Chatbot")
+st.title("News Chatbot")
 
 # API Keys
 SCRAPINGBEE_API_KEY = "U3URPLPZWZ3QHVGEEP5HTXJ95873G9L58RJ3EHS4WSYTXOZAIE71L278CF589042BBMKNXZTRY23VYPF"
